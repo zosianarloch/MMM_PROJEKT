04aa27b75e18fa08522941811c86e16ff74fa6ad
s198232 <145277575+s198232@users.noreply.github.com>
2025-04-05 16:35:31 +0200

    second

--- mmm_projekt.py
+++ mmm_projekt.py
@@ -1,6 +1,15 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+Lp=np.array([])
+Mp=np.array([])
+
+Lc=np.array([])
+Mc=np.array([])
+
+Lo=Lp*Lc
+Mo=Mp*Mc
+
 
 x = np.array([[1, 2, 3], [4, 5, 6]])
 
