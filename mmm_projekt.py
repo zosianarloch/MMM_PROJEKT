02e45801df496563eeebc01fdb02f1756233b267
s198232 <145277575+s198232@users.noreply.github.com>
2025-04-22 20:14:58 +0200

    ..

--- mmm_projekt.py
+++ mmm_projekt.py
@@ -2,18 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a0, a1 =1, 0
+b0, b1, b2 = 0, 1, 2
+c0, c1, c2 = 1, 1, 0
+d0, d1, d2 = 4, 4, 1
 
 Lp=np.array([a1, a0])
-Mp=np.array([])
+Mp=np.array([b2, b1, b0])
 
-Lc=np.array([])
-Mc=np.array([])
-
-
-
-x = np.array([[1, 2, 3], [4, 5, 6]])
-
-print(x)
+Lc=np.array([c2, c1, c0])
+Mc=np.array([d2, d1, d0])
 print(Lp)
 
 
